715ab034fabfcf2e24ac9c250df172d76c2f944b
Alex Ball <alex@cyvl.ai>
2026-01-14 09:12:24 -0500

    update keyboard

---
+++ keyboards/nullbitsco/snap/keymaps/alexjball/config.h
@@ -0,0 +1,9 @@
+// Copyright 2021 Jay Greco
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Reduce VIA layers to fit encoder map in EEPROM
+// Default is 4 layers, but we only use 2
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
+
